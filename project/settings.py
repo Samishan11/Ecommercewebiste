85047758a65658f8ca4fd3000c0895ad9c4492a8
Samishan Thapa Chhetri <60726548+Samishan11@users.noreply.github.com>
2023-07-16 18:03:10 +0545

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -132,8 +132,8 @@ STATICFILES_DIRS = [
 ]
 # stmp 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'joker.shan99@gmail.com'
-EMAIL_HOST_PASSWORD = 'helloworld@@11'
+EMAIL_HOST_USER = 'your email'
+EMAIL_HOST_PASSWORD = 'your pasword'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
